bb15b1ea4f973860fba9a647d6d22b8889b338d4
David Ebner <2010653205@stud.fh-kufstein.ac.at>
2022-05-10 10:52:06 +0200

    added routing for journal and balance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BalanceComponent } from './balance/balance.component';
+import { JournalComponent } from './journal/journal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/balance', pathMatch: 'full'},
+  {path: 'balance', component: BalanceComponent},
+  {path: 'journal', component: JournalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
