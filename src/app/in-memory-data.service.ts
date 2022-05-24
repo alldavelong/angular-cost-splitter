ad9177afadf94f53ecd1ef7c0f594372148b1b3b
David Ebner <2010653205@stud.fh-kufstein.ac.at>
2022-05-24 17:35:21 +0200

    updated demo data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -8,14 +8,14 @@ export class InMemoryDataService implements InMemoryDbService {
 
   createDb() {
     const expenses = [
-      { id: 1, date: '2022-04-23', spentBy: 'Anton', description: 'Brot', amount: 300 },
-      { id: 2, date: '2022-04-23', spentBy: 'Walter', description: 'Milch', amount: 200 },
-      { id: 3, date: '2022-04-25', spentBy: 'Xaver', description: 'Honig', amount: 400 },
-      { id: 4, date: '2022-05-04', spentBy: 'Dora', description: 'Shirt', amount: 5000 }
+      { id: 1, date: '2022-04-23', spentBy: 'Anton', description: 'Brote', amount: 1300 },
+      { id: 2, date: '2022-04-23', spentBy: 'Walter', description: 'Milch', amount: 400 },
+      { id: 3, date: '2022-04-25', spentBy: 'Xaver', description: 'Honig', amount: 1000 },
+      { id: 4, date: '2022-05-04', spentBy: 'Dora', description: 'Heizöl', amount: 5000 }
     ];
     const payments = [
-      { id: 1, date: '2022-05-01', spentBy: 'Anton', amount: 300, receivedBy: 'Xaver' },
-      { id: 2, date: '2022-04-28', spentBy: 'Dora', amount: 5000, receivedBy: 'Walter' }
+      { id: 1, date: '2022-04-28', spentBy: 'Walter', amount: 200, receivedBy: 'Anton' },
+      { id: 2, date: '2022-05-10', spentBy: 'Xaver', amount: 1200, receivedBy: 'Dora' }
     ];
     const users = [
       { id: 1, name: 'Anton' },
