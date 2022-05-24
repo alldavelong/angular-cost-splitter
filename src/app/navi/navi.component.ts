cdcbb774424109715b398e96755f912b1fe763dc
David Ebner <2010653205@stud.fh-kufstein.ac.at>
2022-05-24 16:36:46 +0200

    made navi show state

--- src/app/navi/navi.component.ts
+++ src/app/navi/navi.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NaviComponent implements OnInit {
 
+  navLinks = [
+    {path: '/balance', label: 'Bilanz'},
+    {path: '/journal', label: 'Journal'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
